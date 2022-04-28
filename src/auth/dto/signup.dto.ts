9f5f98e52fd88f0fd8c492adf6f99a9e275ab1cb
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2022-04-29 08:52:19 +0900

    📝 Add dto swagger
    dto에 swagger 작성

--- src/auth/dto/signup.dto.ts
+++ src/auth/dto/signup.dto.ts
@@ -1,21 +1,39 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsIn, IsString, MaxLength, MinLength } from 'class-validator';
 
 export class SignupDto {
+  @ApiProperty({ title: '이메일', example: 'example@example.com' })
   @IsEmail()
   @IsString()
   email: string;
 
+  @ApiProperty({
+    title: '실명',
+    example: '홍길동',
+    description: '티모 Q',
+  })
   @IsString()
   name: string;
 
+  @ApiProperty({ title: '성별', description: 'M 또는 W', example: 'M' })
   @IsIn(['M', 'W'])
   gender: 'M' | 'W';
 
+  @ApiProperty({
+    title: '비밀번호',
+    example: '12345678',
+    description: '8글자 ~ 30글자 까지',
+  })
   @IsString()
   @MinLength(8)
   @MaxLength(30)
   password: string;
 
+  @ApiProperty({
+    title: '비밀번호 확인',
+    example: '12345678',
+    description: '8글자 ~ 30글자 까지',
+  })
   @IsString()
   @MinLength(8)
   @MaxLength(30)
--- src/auth/dto/verify.dto.ts
+++ src/auth/dto/verify.dto.ts
@@ -1,6 +1,8 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsString } from 'class-validator';
 
 export class verifyDto {
+  @ApiProperty({ title: '이메일', example: 'example@example.com' })
   @IsEmail()
   @IsString()
   email: string;
