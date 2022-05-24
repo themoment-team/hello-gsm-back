7e6a406ceb49eefa93b6f26055efbc87d1bb976e
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2022-05-25 00:16:24 +0900

    ✨ Add getUserState
    유저 상태 조회 기능 추가

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { User } from 'src/auth/decorators/user.decorator';
+import { UserService } from './user.service';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private userService: UserService) {}
+
+  @Get()
+  async getUserState(@User('user_idx') user_idx: number) {
+    return this.userService.getUserState(user_idx);
+  }
+}
