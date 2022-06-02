62f0675f487b3727bae500e1644e71e6fdc4a729
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2022-06-02 09:37:29 +0900

    🐛 Update dto/index.ts
    index에서 export 하는거 깜빡함

--- src/application/dto/index.ts
+++ src/application/dto/index.ts
@@ -1,2 +1,5 @@
 export * from './first.submission.dto';
 export * from './seconds.submission.dto';
+export * from './user.dto';
+export * from './application.dto';
+export * from './application.dtail.dto';
