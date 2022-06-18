215bdb58880ba27f23db018c8422d62591793cba
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2022-06-18 23:42:43 +0900

    Add login feature

--- apps/admin/src/auth/auth.module.ts
+++ apps/admin/src/auth/auth.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { PrismaService } from 'apps/client/src/prisma/prisma.service';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 
 @Module({
+  imports: [JwtModule.register({})],
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService, PrismaService],
 })
 export class AuthModule {}
