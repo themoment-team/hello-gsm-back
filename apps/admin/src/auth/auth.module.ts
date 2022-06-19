a961f436e25bc61d81304f976b6d86c10aa1f6cd
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2022-06-19 13:16:05 +0900

    Setting test code

--- apps/admin/src/auth/auth.module.ts
+++ apps/admin/src/auth/auth.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
+import { PrismaModule } from 'apps/client/src/prisma/prisma.module';
 import { PrismaService } from 'apps/client/src/prisma/prisma.service';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 
 @Module({
-  imports: [JwtModule.register({})],
+  imports: [JwtModule.register({}), PrismaModule],
   controllers: [AuthController],
   providers: [AuthService, PrismaService],
 })
--- apps/admin/src/auth/auth.service.spec.ts
+++ apps/admin/src/auth/auth.service.spec.ts
@@ -1,15 +1,21 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import { PrismaService } from 'apps/client/src/prisma/prisma.service';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule } from '@nestjs/config';
 
 describe('AuthService', () => {
   let service: AuthService;
+  let prisma: PrismaService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      imports: [JwtModule.register({}), ConfigModule],
+      providers: [AuthService, PrismaService],
     }).compile();
 
     service = module.get<AuthService>(AuthService);
+    prisma = module.get<PrismaService>(PrismaService);
   });
 
   it('should be defined', () => {
